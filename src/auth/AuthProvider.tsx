1353063af09f69bf402a8295e4eb071d46c97988
Gagun45 <selyanchyn45@gmail.com>
2025-07-03 23:49:02 +0300

    redirect uris added

--- src/auth/AuthProvider.tsx
+++ src/auth/AuthProvider.tsx
@@ -5,7 +5,7 @@ const AuthProvider = ({ children }: { children: ReactNode }) => {
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
-  const redirectUri = import.meta.env.VITE_AUTH0_REDIRECT_URI;
+  const redirectUri = import.meta.env.VITE_AUTH0_LOGIN_REDIRECT_URI;
   console.log("auth provider");
 
   if (!domain || !clientId || !audience || !redirectUri) {
--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -2,13 +2,18 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useUserStore } from "@/stores/useUserStore";
 import { Button } from "./ui/button";
 
-// const REDIRECT_URI = import.meta.env.VITE_AUTH0_REDIRECT_URI;
+const LOGOUT_REDIRECT_URL = import.meta.env.VITE_AUTH0_LOGOUT_REDIRECT_URI;
 
 const AuthButton = () => {
   const { userData } = useUserStore();
   const { loginWithRedirect, logout } = useAuth0();
   return userData ? (
-    <Button variant={"destructive"} onClick={() => logout()}>
+    <Button
+      variant={"destructive"}
+      onClick={() =>
+        logout({ logoutParams: { returnTo: LOGOUT_REDIRECT_URL } })
+      }
+    >
       Logout
     </Button>
   ) : (
